9fb257d1f484beb86d6eb091c3c26d4a4197362c
Alexander Riccio <test35965+learn@gmail.com>
2019-06-12 18:27:28 -0400

    22 passing, 5 failures

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,9 @@
 require_relative 'environment'
-
+require "pry"
 db = SQLite3::Database.new('../db/lab_database.db')
 sql_runner = SQLRunner.new(db)
 
 sql_runner.execute_sample_sql
+
+binding.pry
+0
